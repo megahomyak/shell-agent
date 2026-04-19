c1390044c8de6aa114687e5f825df96ca53756c2
megahomyak <g.megahomyak@gmail.com>
2026-04-19 20:37:32 +0500

    Okay, I will make the LLM write the complex code, and I myself should focus on simple stuff

--- shell-agent.mjs
+++ shell-agent.mjs
@@ -1,30 +1,41 @@
 import * as process from "node:process";
+import * as groq from "groq-sdk";
+import * as fs from "node:fs";
 
 let runPass = async (startingPrompt, complete, execute, history) => {
     let completion = "";
+    process.stdout.write("$ ");
     for await (let part of complete(startingPrompt, history)) {
-        process.stdout.write(part);
+        process.stdout.write(part.replace("\n", "$ \n"));
         completion += part;
     }
+    process.stdout.write("\n");
     let execution = "";
+    process.stdout.write("< ");
     for await (let part of execute(completion)) {
-        process.stdout.write(part);
+        process.stdout.write(part.replace("\n", "< \n"));
         execution += part;
     }
+    process.stdout.write("\n");
     return { completion, execution };
 };
 
-let parseHistory = (history) => {
-    let history = [];
-    for (let block of history.split("\n\n")) {
-        if (!block) continue;
-        let role = block[0] == "$" ? "assistant" : "user";
-        let contentLines = [];
-        for (let line in block.split("\n")) {
-            contentLines.push(line.substring(2));
-        }
-        let content = contentLines.join("\n");
-        history.push({ role, content });
+let runLoop = async (startingPrompt, complete, execute, history) => {
+    while (true) {
+        runPass(startingPrompt, complete, execute, history);
     }
-    return history;
 };
+
+{
+    let groq = new groq.Groq();
+    let startingPrompt = fs.readFileSync("starting_prompt.txt", "utf-8");
+    let complete = async (groq, history) => {
+        return groq.chat.completions.create({
+            model: "llama-3.3-70b-versatile",
+            messages: history,
+            stream: true,
+        });
+    };
+    let execute 
+    runLoop(startingPrompt, complete, execute, history)
+}
