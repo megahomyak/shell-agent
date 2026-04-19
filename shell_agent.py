bb7621f68d214d5bf8d971f7a43ce314109cc668
megahomyak <g.megahomyak@gmail.com>
2026-04-20 00:50:57 +0500

    shell_agent.py is in a decent state

--- shell_agent.py
+++ shell_agent.py
@@ -1,70 +1,39 @@
-import json, subprocess, os, time, re
-from groq import Groq, APIStatusError
-from json.decoder import JSONDecodeError
+import subprocess, json, groq as groq_sdk
 
-def run_agent_loop(starting_prompt, history, complete, execute):
+def run_agent(prompt, history, complete, execute, remember):
     while True:
-        completion = ""
-        with complete(starting_prompt, history) as chunks:
-            print("$ ", end="", flush=True)
-            for chunk in chunks:
-                completion += chunk
-                print(chunk.replace("\n", "\n$ "), end="", flush=True)
-            print()
-        print("$ " + completion.replace("\n", "\n$ ") + "\n")
+        completion = complete(prompt, history)
         execution = execute(completion)
-        print("< " + execution.replace("\n", "\n< ") + "\n")
-        history.append({"completion": completion, "execution": execution})
+        remember(history, completion, execution)
 
-STARTING_PROMPT = """
-You are an AI assistant whose every message is directly passed to a new Bash session, one session per message.
-
-Your goal is, using the Bash shell you are given, to start listening on a Telegram bot with the token {telegram_token}, and do whatever is ordered from you
-
-Your messages are passed directly to Bash, so if you want to think, you have to think inside of Bash comments. Your messages should be Bash scripts executable as is
-
-You are writing plain text, not Markdown, so do NOT add code fences (```) around your code
-""".strip()
-
-def run():
-    starting_prompt = STARTING_PROMPT.format(**json.load(open("prompt_config.json")))
-    history = []
-    groq = Groq()
-    def complete(starting_prompt, history):
-        messages = [
-            {
-                "role": "system",
-                "content": starting_prompt
-            },
-        ]
-        for turn in history:
-            messages.append({
-                "role": "assistant",
-                "content": turn["completion"],
-            })
-            messages.append({
-                "role": "user",
-                "content": turn["execution"],
-            })
-        try:
-            completion = groq.chat.completions.create(
-                model="openai/gpt-oss-120b",
-                messages=messages,
-            )
-        except APIStatusError as e:
-            if e.status_code == 413: # Context too long
-                print(e)
-                history[:] = history[1:]
-                return complete(starting_prompt, history)
-            elif e.status_code == 429: # Rate limited
-                print(e)
-                time.sleep(10)
-                return complete(starting_prompt, history)
+def main():
+    with open("prompt.txt") as f: prompt = f.read()
+    try:
+        with open("history.json") as f: history = json.load(f)
+    except FileNotFoundError: history = []
+    groq = groq_sdk.Groq()
+    def complete(prompt, history):
+        while True:
+            try:
+                completion = groq.chat.completions.create(
+                    model="llama-3.3-70b-versatile",
+                    messages=[{"role": "system", "content": prompt}] + history,
+                ).choices[0].message.content
+            except groq_sdk.APIStatusError as e:
+                unexpected = False
+                if e.response.reason_phrase == "Payload Too Large":
+                    history[:] = history[2:]
+                else:
+                    unexpected = True
+                if unexpected:
+                    raise e
+                else:
+                    print("ERROR:\n" + str(e) + "\n")
             else:
-                raise e
-        return completion.choices[0].message.content
+                print("COMPLETION:\n" + completion + "\n")
+                return completion
     def execute(completion):
-        return subprocess.run([
+        execution = subprocess.run([
             "docker",
             "exec",
             "shell-agent",
@@ -72,6 +41,14 @@ def run():
             "-c",
             completion,
         ], stderr=subprocess.STDOUT, stdout=subprocess.PIPE, stdin=subprocess.DEVNULL).stdout.decode()
-    run_agent_loop(starting_prompt, history, complete, execute)
+        print("EXECUTION:\n" + execution + "\n")
+        return execution
+    def remember(history, completion, execution):
+        history.extend([
+            {"role": "assistant", "content": completion},
+            {"role": "user", "content": execution},
+        ])
+        with open("history.json", "w") as f: json.dump(history, f, indent=0)
+    run_agent(prompt, history, complete, execute, remember)
 
-run()
+main()
